fc595b87e438efe99b5b33aa48f4498e39ad513b
StPayne55 <Stpayne55@gmail.com>
2014-08-21 15:35:59 -0400

    Update DEMOFirstViewController.m

--- RESideMenuStoryboardsExample/DEMOFirstViewController.m
+++ RESideMenuStoryboardsExample/DEMOFirstViewController.m
@@ -9,9 +9,52 @@
 #import "DEMOFirstViewController.h"
 
 @interface DEMOFirstViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *blueImage;
+@property (weak, nonatomic) IBOutlet UIImageView *pinkImage;
+@property (weak, nonatomic) IBOutlet UIView *loginComponents;
+@property (weak, nonatomic) IBOutlet UILabel *logo;
+
+@property (weak, nonatomic) IBOutlet UILabel *lblLogin;
 
 @end
 
 @implementation DEMOFirstViewController
+CGPoint blueOrigin;
+CGPoint pinkOrigin;
+CGPoint loginOrigin;
+CGPoint logoOrigin;
+
+-(void)viewDidLoad{
+    _loginComponents.alpha = 0;
+    _logo.alpha = 0;
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    blueOrigin = _blueImage.center;
+    pinkOrigin = _pinkImage.center;
+    loginOrigin = _loginComponents.center;
+    logoOrigin = _logo.center;
+    
+    _blueImage.center = CGPointMake(-600, _blueImage.center.y);
+    _pinkImage.center = CGPointMake(600, _pinkImage.center.y);
+    _loginComponents.center = CGPointMake(_loginComponents.center.x, 900);
+    _logo.center = CGPointMake(_logo.center.x, -600);
+}
+-(void)viewDidAppear:(BOOL)animated{
+    _logo.alpha = 1;
+    [UIView animateWithDuration:1 delay:0 usingSpringWithDamping:.5 initialSpringVelocity:.9 options:UIViewAnimationCurveEaseInOut animations:^{
+        _logo.center = logoOrigin;
+        _blueImage.center = blueOrigin;
+        _pinkImage.center = pinkOrigin;
+        _loginComponents.alpha = 1;
+    }completion:^(BOOL finished) {
+        [UIView animateWithDuration:.5 delay:0 usingSpringWithDamping:.5 initialSpringVelocity:.9 options:nil animations:^{
+            _loginComponents.center = loginOrigin;
+        }completion:nil];
+    }];
+    
+}
 
 @end
